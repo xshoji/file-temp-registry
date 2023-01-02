926c8731bbd647425398b14eeca2b8be78b67612
xshoji <xshoji@users.noreply.github.com>
2023-01-02 20:02:11 +0900

    fix

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ var (
 	// Define application logic variables.
 	fileRegistryMap = map[string]FileRegistry{}
 	mutex           sync.Mutex
-	// 時刻と時刻のマイクロ秒、ディレクトリパスを含めたファイル名を出力
+	// Define logger: date, time, microseconds, directory and file path are always outputted.
 	logger         = log.New(os.Stdout, "[Logger] ", log.Llongfile|log.LstdFlags)
 	loggerLogLevel = Debug
 )
@@ -56,7 +56,7 @@ func logging(loglevel LogLevel, logLogger *log.Logger, v ...interface{}) {
 		case Debug:
 			return "Debug"
 		default:
-			return ""
+			return "Unknown"
 		}
 	}()
 	logLogger.Println(append([]interface{}{"[" + level + "]"}, v...)...)
@@ -94,86 +94,15 @@ func main() {
 	loggerLogLevel = LogLevel(*argsLogLevel)
 
 	//-------------------------
-	// 各種パスの処理
+	// 各パスの処理
 	// upload
-	http.HandleFunc(UrlPathPrefix+"/upload", func(w http.ResponseWriter, r *http.Request) {
-		logging(Debug, logger, r.RemoteAddr, r.RequestURI, r.Header)
-		// - [How can I handle http requests of different methods to / in Go? - Stack Overflow](https://stackoverflow.com/questions/15240884/how-can-i-handle-http-requests-of-different-methods-to-in-go)
-		if allowedHttpMethod := http.MethodPost; r.Method != allowedHttpMethod {
-			responseJson(w, 405, `{"message":"Method Not Allowed. (Only `+allowedHttpMethod+` is allowed)"}`)
-			return
-		}
-
-		// go - golang - How to check multipart.File information - Stack Overflow
-		// https://stackoverflow.com/questions/17129797/golang-how-to-check-multipart-file-information
-		if err := r.ParseMultipartForm(*argsMaxFileSize * MB); err != nil {
-			responseJson(w, 400, `{"message":"`+err.Error()+`"}`)
-			return
-		}
-
-		r.Body = http.MaxBytesReader(w, r.Body, *argsMaxFileSize*MB)
-
-		key := r.FormValue("key")
-		expiryTimeMinutes := r.FormValue("expiryTimeMinutes")
-		fileExpirationMinutes := *argsFileExpiration
-		// expiryTimeMinutesで指定された数値がintにキャストできる値だった場合は、ファイルの期限を指定分に設定する
-		if expiryTimeMinutesInt, err := strconv.Atoi(expiryTimeMinutes); err == nil {
-			fileExpirationMinutes = expiryTimeMinutesInt
-		}
-		file, fileHeader, _ := r.FormFile("file")
-		mutex.Lock()
-		defer func() { mutex.Unlock() }()
-		fileRegistryMap[key] = FileRegistry{
-			key:                 key,
-			expiryTimeMinutes:   expiryTimeMinutes,
-			expiredAt:           time.Now().Add(time.Duration(fileExpirationMinutes) * time.Minute),
-			multipartFile:       file,
-			multipartFileHeader: fileHeader,
-		}
-		responseBody := `{"message":"` + fileRegistryMap[key].String() + `"}`
-		logging(Debug, logger, responseBody)
-		responseJson(w, 200, responseBody)
-	})
-
+	http.HandleFunc(UrlPathPrefix+"/upload", handleUpload)
 	// download
-	http.HandleFunc(UrlPathPrefix+"/download", func(w http.ResponseWriter, r *http.Request) {
-		logging(Debug, logger, r.RemoteAddr, r.RequestURI, r.Header)
-		if allowedHttpMethod := http.MethodGet; r.Method != allowedHttpMethod {
-			responseJson(w, 405, `{"message":"Method Not Allowed. (Only `+allowedHttpMethod+` is allowed)"}`)
-			return
-		}
-		key := r.URL.Query().Get("key")
-		mutex.Lock()
-		defer func() { mutex.Unlock() }()
-		if _, ok := fileRegistryMap[key]; !ok {
-			responseJson(w, 404, `{"message":"file not found."}`)
-			return
-		}
-		logging(Debug, logger, fileRegistryMap[key].String())
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", fileRegistryMap[key].multipartFileHeader.Header.Get("Content-Type"))
-		w.Header().Set("Content-Disposition", "attachment; filename="+fileRegistryMap[key].multipartFileHeader.Filename)
-		io.Copy(w, fileRegistryMap[key].multipartFile)
-		// reset
-		fileRegistryMap[key].multipartFile.Seek(0, io.SeekStart)
-	})
+	http.HandleFunc(UrlPathPrefix+"/download", handleDownload)
 
-	// 期限切れのファイルを削除するgoroutine
-	go func() {
-		for {
-			time.Sleep(time.Minute * 1)
-			func() {
-				mutex.Lock()
-				defer func() { mutex.Unlock() }()
-				for key, fileRegistry := range fileRegistryMap {
-					if fileRegistry.expiredAt.Before(time.Now()) {
-						logging(Debug, logger, "[File cleaner goroutine] File expired. >>", fileRegistry.String())
-						delete(fileRegistryMap, key)
-					}
-				}
-			}()
-		}
-	}()
+	//-------------------------
+	// 期限切れのファイルを削除するgoroutineを起動
+	go cleanExpiredFile()
 
 	//-------------------------
 	// Listen開始
@@ -190,6 +119,86 @@ func main() {
 	err = http.ListenAndServe(":"+strconv.Itoa(*argsPort), nil)
 	if err != nil {
 		logging(Panic, logger, "ListenAndServe:", err)
+	}
+}
+
+// Upload処理：POSTされたファイルをアプリ内部のmapに保持する
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	logging(Debug, logger, r.RemoteAddr, r.RequestURI, r.Header)
+	// - [How can I handle http requests of different methods to / in Go? - Stack Overflow](https://stackoverflow.com/questions/15240884/how-can-i-handle-http-requests-of-different-methods-to-in-go)
+	if allowedHttpMethod := http.MethodPost; r.Method != allowedHttpMethod {
+		responseJson(w, 405, `{"message":"Method Not Allowed. (Only `+allowedHttpMethod+` is allowed)"}`)
+		return
+	}
+
+	// go - golang - How to check multipart.File information - Stack Overflow
+	// https://stackoverflow.com/questions/17129797/golang-how-to-check-multipart-file-information
+	if err := r.ParseMultipartForm(*argsMaxFileSize * MB); err != nil {
+		responseJson(w, 400, `{"message":"`+err.Error()+`"}`)
+		return
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, *argsMaxFileSize*MB)
+
+	key := r.FormValue("key")
+	expiryTimeMinutes := r.FormValue("expiryTimeMinutes")
+	fileExpirationMinutes := *argsFileExpiration
+	// expiryTimeMinutesで指定された数値がintにキャストできる値だった場合は、ファイルの期限を指定分に設定する
+	if expiryTimeMinutesInt, err := strconv.Atoi(expiryTimeMinutes); err == nil {
+		fileExpirationMinutes = expiryTimeMinutesInt
+	}
+	file, fileHeader, _ := r.FormFile("file")
+	mutex.Lock()
+	defer func() { mutex.Unlock() }()
+	fileRegistryMap[key] = FileRegistry{
+		key:                 key,
+		expiryTimeMinutes:   expiryTimeMinutes,
+		expiredAt:           time.Now().Add(time.Duration(fileExpirationMinutes) * time.Minute),
+		multipartFile:       file,
+		multipartFileHeader: fileHeader,
+	}
+	responseBody := `{"message":"` + fileRegistryMap[key].String() + `"}`
+	logging(Debug, logger, responseBody)
+	responseJson(w, 200, responseBody)
+}
+
+// Download処理：key指定されたファイルをレスポンスする
+func handleDownload(w http.ResponseWriter, r *http.Request) {
+	logging(Debug, logger, r.RemoteAddr, r.RequestURI, r.Header)
+	if allowedHttpMethod := http.MethodGet; r.Method != allowedHttpMethod {
+		responseJson(w, 405, `{"message":"Method Not Allowed. (Only `+allowedHttpMethod+` is allowed)"}`)
+		return
+	}
+	key := r.URL.Query().Get("key")
+	mutex.Lock()
+	defer func() { mutex.Unlock() }()
+	if _, ok := fileRegistryMap[key]; !ok {
+		responseJson(w, 404, `{"message":"file not found."}`)
+		return
+	}
+	logging(Debug, logger, fileRegistryMap[key].String())
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", fileRegistryMap[key].multipartFileHeader.Header.Get("Content-Type"))
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileRegistryMap[key].multipartFileHeader.Filename)
+	io.Copy(w, fileRegistryMap[key].multipartFile)
+	// reset
+	fileRegistryMap[key].multipartFile.Seek(0, io.SeekStart)
+}
+
+// 期限切れのファイルをお掃除する
+func cleanExpiredFile() {
+	for {
+		time.Sleep(time.Minute * 1)
+		func() {
+			mutex.Lock()
+			defer func() { mutex.Unlock() }()
+			for key, fileRegistry := range fileRegistryMap {
+				if fileRegistry.expiredAt.Before(time.Now()) {
+					logging(Debug, logger, "[File cleaner goroutine] File expired. >>", fileRegistry.String())
+					delete(fileRegistryMap, key)
+				}
+			}
+		}()
 	}
 }
 
